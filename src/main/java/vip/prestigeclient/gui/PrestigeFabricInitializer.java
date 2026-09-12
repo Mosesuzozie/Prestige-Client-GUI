46a2e00c4db36f97ba09dd13d59c78b219745142
Mosesuzozie <mosesuzozie28@gmail.com>
2026-09-12 14:59:36 -0400

    Implement PrestigeFabricInitializer for GUI handling

---
+++ src/main/java/vip/prestigeclient/gui/PrestigeFabricInitializer.java
@@ -0,0 +1,31 @@
+package vip.prestigeclient.gui;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class PrestigeFabricInitializer implements ClientModInitializer {
+    private static KeyBinding openGuiBind;
+    private static PrestigeClickGUI guiInstance;
+
+    @Override
+    public void onInitializeClient() {
+        openGuiBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.prestigeclient.opengui",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "category.prestigeclient.binds"
+        ));
+
+        guiInstance = new PrestigeClickGUI();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGuiBind.wasPressed()) {
+                System.out.println("Prestige ClickGUI Toggle Action Triggered!");
+            }
+        });
+    }
+}
