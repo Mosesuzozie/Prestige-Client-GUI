39fa071dd7cf003f040a6134fefd13f91afba505
Mosesuzozie <mosesuzozie28@gmail.com>
2026-09-12 17:01:47 -0400

    Refactor key input handling in MixinKeyboardInput

--- src/main/java/vip/prestigeclient/mixin/MixinKeyboardInput.java
+++ src/main/java/vip/prestigeclient/mixin/MixinKeyboardInput.java
@@ -2,6 +2,7 @@ package vip.prestigeclient.mixin;
 
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.input.KeyInput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +12,19 @@ import vip.prestigeclient.gui.PrestigeClickGUI;
 @Mixin(Keyboard.class)
 public class MixinKeyboardInput {
 
-    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
-    private void onKeyInputPass(long window, int key, int scancode, int action, int mods, CallbackInfo ci) {
+    @Inject(method = "onKey", at = @At("HEAD"))
+    private void onKeyInputPass(
+            long window,
+            int action,
+            KeyInput input,
+            CallbackInfo ci
+    ) {
         MinecraftClient mc = MinecraftClient.getInstance();
+
         if (action == 1 && mc.player != null) {
-            PrestigeClickGUI.handleKeyPressEvent(key);
+            PrestigeClickGUI.handleKeyPressEvent(
+                    input.getKeycode()
+            );
         }
     }
 }
