dd30213e2c7032821cfa3019c4fc398109462f39
Mosesuzozie <mosesuzozie28@gmail.com>
2026-09-12 17:01:17 -0400

    Refactor key binding registration and category

--- src/main/java/vip/prestigeclient/gui/PrestigeFabricInitializer.java
+++ src/main/java/vip/prestigeclient/gui/PrestigeFabricInitializer.java
@@ -8,23 +8,29 @@ import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class PrestigeFabricInitializer implements ClientModInitializer {
+
     private static KeyBinding openGuiBind;
     private static PrestigeClickGUI guiInstance;
 
     @Override
     public void onInitializeClient() {
-        openGuiBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.prestigeclient.opengui",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_RIGHT_SHIFT,
-            "category.prestigeclient.binds"
-        ));
+
+        openGuiBind = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.prestigeclient.opengui",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_RIGHT_SHIFT,
+                        KeyBinding.Category.MISC
+                )
+        );
 
         guiInstance = new PrestigeClickGUI();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openGuiBind.wasPressed()) {
-                System.out.println("Prestige ClickGUI Toggle Action Triggered!");
+                System.out.println(
+                        "Prestige ClickGUI Toggle Action Triggered!"
+                );
             }
         });
     }
